ab2baf6e650d8f637570544e8d811a1ca0523a31
Deraldo-Neto <deraldoalmeidaneto@gmail.com>
2022-05-25 12:32:57 -0700

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Deraldo on 25/05/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
