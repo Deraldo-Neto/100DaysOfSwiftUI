290f806a79023ef4da0ce5119151b6046992ecaa
Deraldo Neto <57962710+Deraldo-Neto@users.noreply.github.com>
2023-04-25 15:28:50 -0700

    Add files via upload

---
+++ Day 19/Day 19/Day_19App.swift
@@ -0,0 +1,17 @@
+//
+//  Day_19App.swift
+//  Day 19
+//
+//  Created by Deraldo on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Day_19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
